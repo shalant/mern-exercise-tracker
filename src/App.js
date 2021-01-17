d83ea9b4d6f24028f238030efa19e1ba19acb3c0
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-16 18:42:28 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Exercise from '../backend/models/exercise.model';
+//import Exercise from '../backend/models/exercise.model';
 
 import Navbar from './components/navbar.component';
-import ExerciseList from './components/exercise-list.component';
-import EditExercise from './components/edit-exercise.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercises from './components/edit-exercises.component';
 import CreateExercise from './components/create-exercise.component';
 import CreateUser from './components/create-user.component';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-        <br />
-        <Route path='/' exact component={ExerciseList} />
-        <Route path='/edit/:id' exact component={EditExercise} />
-        <Route path='/create' exact component={CreateExercise} />
-        <Route path='/user' exact component={CreateUser} />
-
+      <div className='container'>
+        <Navbar />
+          <br />
+          <Route path='/' exact component={ExercisesList} />
+          <Route path='/edit/:id' exact component={EditExercises} />
+          <Route path='/create' exact component={CreateExercise} />
+          <Route path='/user' exact component={CreateUser} />
+      </div>
     </Router>
   );
 }
