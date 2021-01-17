33cfc0c9964d1fa925f60f524f1da446d6eca8d8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-16 22:14:03 -0600

    create exercise finished

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateExercises extends Component {
     constructor(props) {
         super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
             username: '',
@@ -11,6 +19,13 @@ export default class CreateExercises extends Component {
             date: new Date(),
             users: []
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        });
     }
 
     onChangeUsername(e) {
@@ -31,7 +46,7 @@ export default class CreateExercises extends Component {
         });
     }
     
-    onChangeDate(e) {
+    onChangeDate(date) {
         this.setState({
             date: date
         });
@@ -48,12 +63,64 @@ export default class CreateExercises extends Component {
         }
 
         console.log(exercise)
+
+        //take person back to homepage (list of exercises)
+        window.location = '/';
     }
 
     render() {
         return (
             <div>
-                <p>You are on the Create Exercises Component</p>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Username: </label>
+                        <select ref='userInput'
+                            required
+                            className='form-control'
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}>
+                            {
+                                this.state.users.map(function(user) {
+                                    return <option
+                                        key={user}
+                                        value={user}>{user}
+                                        </option>
+                                })
+                            }
+                            </select>
+                    </div>
+                    <div className='form-group'>
+                        <label>Description: </label>
+                        <input type='text'
+                            required
+                            className='form-control'
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                            />
+                    </div>
+                    <div className='form-group'>
+                        <label>Duration (in minutes): </label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            value={this.state.duration}
+                            onChange={this.onChangeDuration}
+                            />
+                    </div>
+                    <div className='form-group'>
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+                    <div className='form-group'>
+                        <input type='submit' value='Create Exercise Log' className='btn btn-primary' />
+                    </div>
+                </form>
             </div>
         )
     }
