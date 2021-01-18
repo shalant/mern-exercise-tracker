3b389a9d4ebc07a21fd8ff2fb8744bf58e012f74
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-17 21:00:29 -0600

    first connecting both ends

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-export default class CreateExercises extends Component {
+export default class CreateExercise extends Component {
     constructor(props) {
         super(props);
 
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export default class CreateUsers extends Component {
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-        username = '',
+        username: ''
     }
     
     }
@@ -24,16 +25,35 @@ export default class CreateUsers extends Component {
 
         const user = {
             username: this.state.username,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date
         }
-    }
+        console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
+
+    this.setState({
+        username: ''
+    })
+}
 
     render() {
         return (
             <div>
-                <p>You are on the Create Users Component</p>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Username: </label>
+                        <input type='text'
+                            required
+                            className='form-control'
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input type='submit' value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
