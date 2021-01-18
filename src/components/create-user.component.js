3234e3ebc32042275b58ba6e05c6520ce55c6fd6
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-17 20:32:39 -0600

    more react

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+    
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+        username = '',
+    }
+    
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+    }
+
     render() {
         return (
             <div>
